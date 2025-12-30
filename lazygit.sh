341bf3292b2bc60154dcdeeca3b3fb9e79da3403
hainet50b <20400616+hainet50b@users.noreply.github.com>
2025-12-31 00:58:29 +0900

    Make lazygit install script idempotent and safer

--- lazygit.sh
+++ lazygit.sh
@@ -1,9 +1,27 @@
-GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/jesseduffield/lazygit/releases/latest | jq -r .tag_name | tr -d 'v')
-GITHUB_FILE="lazygit_${GITHUB_LATEST_VERSION}_$(uname -s | tr 'A-Z' 'a-z')"_$(uname -m).tar.gz
-GITHUB_URL="https://github.com/jesseduffield/lazygit/releases/download/v$GITHUB_LATEST_VERSION/$GITHUB_FILE"
+set -eu
 
-curl -L -s -o lazygit.tar.gz $GITHUB_URL
-tar -xvzf lazygit.tar.gz lazygit
-install -Dm 755 lazygit -t ~/.local/bin
-rm lazygit lazygit.tar.gz
+GITHUB_LATEST_VERSION=$(curl -fsSL https://api.github.com/repos/jesseduffield/lazygit/releases/latest | sed -n 's/.*tag_name\": \"v\([^\"]*\).*/\1/p')
+os=$(uname -s | tr 'A-Z' 'a-z')
+arch=$(uname -m)
+
+GITHUB_FILE="lazygit_${GITHUB_LATEST_VERSION}_${os}_${arch}.tar.gz"
+GITHUB_URL="https://github.com/jesseduffield/lazygit/releases/download/v${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
+
+LAZYGIT_CURRENT_VERSION='0.0.0'
+if command -v lazygit > /dev/null 2>&1; then
+  LAZYGIT_CURRENT_VERSION=$(lazygit -v | sed -n 's/.*, version=\([^,]*\).*/\1/p')
+fi
+
+if [ "$LAZYGIT_CURRENT_VERSION" = "$GITHUB_LATEST_VERSION" ]; then
+  exit 0
+fi
+
+tmpdir=$(mktemp -d)
+trap 'rm -rf "$tmpdir"' EXIT
+
+curl -fsSL -o "$tmpdir/lazygit.tar.gz" "$GITHUB_URL"
+tar -xzf "$tmpdir/lazygit.tar.gz" -C "$tmpdir" lazygit
+
+mkdir -p "$HOME/.local/bin"
+install -m 755 "$tmpdir/lazygit" "$HOME/.local/bin/lazygit"
 
